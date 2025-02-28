d7f08277f4bf0cf28b45003d9819f9d310ec0058
gabrieldsv <87916577+gabrieldsv@users.noreply.github.com>
2025-02-28 18:25:20 -0300

    erro 404-

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import StudentList from './pages/students/StudentList';
+import StudentDetail from './pages/students/StudentDetail';
+import StudentForm from './pages/students/StudentForm';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/alunos" element={<StudentList />} />
+        <Route path="/alunos/:id" element={<StudentDetail />} />
+        <Route path="/alunos/:id/editar" element={<StudentForm />} />
+        <Route path="/alunos/novo" element={<StudentForm />} />
+        <Route path="/cursos" element={<CoursesPage />} /> {/* Está faltando? */}
+        <Route path="/" element={<HomePage />} /> {/* Página inicial */}
+        <Route path="*" element={<NotFoundPage />} /> {/* 404 no cliente */}
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
